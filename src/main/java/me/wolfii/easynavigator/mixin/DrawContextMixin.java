779b62faae39c7b353fe5f4ea118ec5ec99d7c5a
JustAlittleWolf <jakobi5000@gmail.com>
2024-07-20 19:48:19 +0200

    prevent creation of empty NBT tags
    fix: prevent creation of empty NBT tags
    
    should resolve #7

--- src/main/java/me/wolfii/easynavigator/mixin/DrawContextMixin.java
+++ src/main/java/me/wolfii/easynavigator/mixin/DrawContextMixin.java
@@ -4,6 +4,7 @@ import me.wolfii.easynavigator.config.Config;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,7 +16,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class DrawContextMixin {
     @Inject(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V", shift = At.Shift.AFTER))
     private void scaleItemRendering(LivingEntity entity, World world, ItemStack stack, int x, int y, int seed, int z, CallbackInfo ci) {
-        NbtCompound nbtCompound = stack.getOrCreateNbt();
+        if (stack.getItem() != Items.COMPASS) return;
+        NbtCompound nbtCompound = stack.getNbt();
+        if (nbtCompound == null) return;
         if (!nbtCompound.contains("CustomScale")) return;
         if (!nbtCompound.getBoolean("CustomScale")) return;
         float customScale = Config.getConfig().scale;
