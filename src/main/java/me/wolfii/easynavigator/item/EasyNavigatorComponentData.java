713707416bde4c97baa5231ef2119ba4e3c43765
JustAlittleWolf <jakobi5000@gmail.com>
2025-06-05 13:58:22 +0200

    feat: update to 1.21.4

--- src/main/java/me/wolfii/easynavigator/item/EasyNavigatorComponentData.java
+++ src/main/java/me/wolfii/easynavigator/item/EasyNavigatorComponentData.java
@@ -8,8 +8,8 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class EasyNavigatorComponentData {
-    public static final ComponentType<Boolean> CUSTOM_SCALE = ComponentType.<Boolean>builder().codec(Codec.BOOL).packetCodec(PacketCodecs.BOOL).build();
-    public static final ComponentType<Boolean> REMOVE_ENCHANTMENT_GLINT = ComponentType.<Boolean>builder().codec(Codec.BOOL).packetCodec(PacketCodecs.BOOL).build();
+    public static final ComponentType<Boolean> CUSTOM_SCALE = ComponentType.<Boolean>builder().codec(Codec.BOOL).packetCodec(PacketCodecs.BOOLEAN).build();
+    public static final ComponentType<Boolean> REMOVE_ENCHANTMENT_GLINT = ComponentType.<Boolean>builder().codec(Codec.BOOL).packetCodec(PacketCodecs.BOOLEAN).build();
 
     public static void register() {
         Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of("easynavigator", "custom_scale"), CUSTOM_SCALE);
